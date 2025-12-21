7cf31e832e620b54b7fb4aab5a30af5c900bdb00
Arnol Garzón <arnol_garzon@hotmail.com>
2025-12-21 10:31:39 -0500

    Configuración para GitHub Pages

--- TUAL-frontend/vite.config.js
+++ TUAL-frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TUAL/', // Asegura que las rutas de los archivos estáticos apunten a la subcarpeta del repo
 })
